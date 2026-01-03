fc7869e3d34fe9e2a1302abec03107287455a6ca
Jenil <jenil.rohit45@gmail.com>
2026-01-03 14:45:58 +0530

    add persistent logic for auth request

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,12 +1,20 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store";
 
 const Dashboard = () => {
+  // protected route logic
+  const { user } = useAuthStore();
+
+  if(user === null) {
+    return <Navigate to={"/auth/login"} replace={true} />
+  }
+
   return (
     <div>
-        <h1>Dashboard Component</h1>
-        <Outlet />
+      <h1>Dashboard Component</h1>
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
