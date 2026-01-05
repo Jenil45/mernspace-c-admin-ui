fc5c833de4ab7a67f0bbee734b5b959dfc28d2f0
Jenil <jenil.rohit45@gmail.com>
2026-01-05 20:56:14 +0530

    manage filter state and add user form drawer layout

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,79 +1,120 @@
-import { Breadcrumb, Space, Table } from "antd"
-import {RightOutlined} from '@ant-design/icons';
+import { Breadcrumb, Button, Drawer, Space, Table } from "antd";
+import { RightOutlined } from "@ant-design/icons";
 import { Link, Navigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import type { User } from "../../types";
 import { getUsers } from "../../http/api";
 import { useAuthStore } from "../../store";
 import UsersFilter from "./UsersFilters";
+import { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
 
 const columns = [
-    {
-        title: 'ID',
-        dataIndex: 'id',
-        key: 'id',
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
     },
-    {
-        title: 'Name',
-        dataIndex: 'firstName',
-        key: 'firstName',
-        render: (_text: string, record: User) => {
-            return (
-                <div>
-                    {record.firstName} {record.lastName}
-                </div>
-            );
-        },
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        key: 'email',
-    },
-    {
-        title: 'Role',
-        dataIndex: 'role',
-        key: 'role',
-    },
-    {
-        title: 'Restaurant',
-        dataIndex: 'tenant',
-        key: 'tenant',
-        // render: (_text: string, record: User) => {
-        //     return <div>{record.tenant?.name}</div>;
-        // },
-    },
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+  {
+    title: "Restaurant",
+    dataIndex: "tenant",
+    key: "tenant",
+    // render: (_text: string, record: User) => {
+    //     return <div>{record.tenant?.name}</div>;
+    // },
+  },
 ];
 
 const Users = () => {
 
-    const {user} = useAuthStore();
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
-    const {data: users, isFetching, isError, error} = useQuery({
-        queryKey: ["users"],
-        queryFn: async () => {
-            const res = await getUsers();
-            console.log("Fetch data: ", res);
-            
-            return res.data;
-        },
-    });
+  const { user } = useAuthStore();
 
-    if (user?.role !== 'admin') {
-        return <Navigate to="/" replace={true} />;
-    }
+  const {
+    data: users,
+    isFetching,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await getUsers();
+      console.log("Fetch data: ", res);
+
+      return res.data;
+    },
+  });
+
+  if (user?.role !== "admin") {
+    return <Navigate to="/" replace={true} />;
+  }
 
   return (
     <>
-        <Space vertical style={{width: "100%"}} size={"large"}>
-            <Breadcrumb separator={<RightOutlined />} items={[{ title: <Link to={"/"}>Dashboard</Link> }, {title: 'Users'}]} />
-            {isFetching && <div>Loading...</div>}
-            {isError && <div>{error.message}</div>}
-            <UsersFilter />
-            <Table  columns={columns} dataSource={users} />
-        </Space>
-    </>
-  )
-}
+      <Space vertical style={{ width: "100%" }} size={"large"}>
+        <Breadcrumb
+          separator={<RightOutlined />}
+          items={[
+            { title: <Link to={"/"}>Dashboard</Link> },
+            { title: "Users" },
+          ]}
+        />
+        {isFetching && <div>Loading...</div>}
+        {isError && <div>{error.message}</div>}
+        <UsersFilter
+          onFilterChange={(filterName: string, filterValue: string) => {
+            console.log(filterName);
+            console.log(filterValue);
+          }}
+        >
+            <Button type="primary" icon={<PlusOutlined />} onClick={() => {setDrawerOpen(true)}}>Add User</Button>
+        </UsersFilter>
+        <Table columns={columns} dataSource={users} rowKey={"id"} />
 
-export default Users
+        <Drawer
+          title={"Create User"}
+          size={720}
+          destroyOnHidden={true}
+          open={drawerOpen}
+          onClose={() => {
+            setDrawerOpen(false);
+            console.log("Closing");
+          }}
+          extra= {
+            <Space>
+                <Button>Cancel</Button>
+                <Button type="primary">Submit</Button>
+            </Space>
+          }
+        >
+          <p>Some content</p>
+        </Drawer>
+      </Space>
+    </>
+  );
+};
+
+export default Users;
--- src/pages/users/UsersFilters.tsx
+++ src/pages/users/UsersFilters.tsx
@@ -1,36 +1,61 @@
-import { Button, Card, Col, Input, Row, Select } from "antd";
-import {PlusOutlined} from "@ant-design/icons";
+import { Card, Col, Input, Row, Select } from "antd";
+import type React from "react";
 
-const UsersFilter = () => {
+type UsersFiltersProps = {
+  children: React.ReactNode;
+  onFilterChange: (filterName: string, filterValue: string) => void;
+};
+
+const UsersFilter = ({ onFilterChange, children }: UsersFiltersProps) => {
   return (
     <Card>
       <Row justify={"space-between"}>
         <Col span={16}>
-            <Row gutter={"20px"}>
-                <Col span={8}>
-                    <Input.Search placeholder="Search.." />
-                </Col>
-                <Col span={8}>
-                    <Select style={{width: '100%'}} allowClear={true} placeholder="Select Role">
-                        <Select.Option value="admin">Admin</Select.Option>
-                        <Select.Option value="manager">Manager</Select.Option>
-                        <Select.Option value="customer">Customer</Select.Option>
-                    </Select>
-                </Col>
-                <Col span={8}>
-                    <Select style={{width: '100%'}} placeholder="Status">
-                        <Select.Option value="ban">Ban</Select.Option>
-                        <Select.Option value="active">Active</Select.Option>
-                    </Select>
-                </Col>
-            </Row>
+          <Row gutter={"20px"}>
+            <Col span={8}>
+              <Input.Search
+                placeholder="Search.."
+                allowClear={true}
+                onChange={(e) =>
+                  onFilterChange("searchFilter", e.target.value)
+                }
+              />
+            </Col>
+            <Col span={8}>
+              <Select
+                style={{ width: "100%" }}
+                allowClear={true}
+                placeholder="Select Role"
+                onChange={(selectedItem) =>
+                  onFilterChange("roleFilter", selectedItem)
+                }
+              >
+                <Select.Option value="admin">Admin</Select.Option>
+                <Select.Option value="manager">Manager</Select.Option>
+                <Select.Option value="customer">Customer</Select.Option>
+              </Select>
+            </Col>
+            <Col span={8}>
+              <Select
+                style={{ width: "100%" }}
+                placeholder="Status"
+                allowClear={true}
+                onChange={(selectedItem) =>
+                  onFilterChange("statusFilter", selectedItem)
+                }
+              >
+                <Select.Option value="ban">Ban</Select.Option>
+                <Select.Option value="active">Active</Select.Option>
+              </Select>
+            </Col>
+          </Row>
         </Col>
-        <Col span={8} style={{display: 'flex', justifyContent: 'end'}}>
-            <Button type="primary" icon={<PlusOutlined />}>Add User</Button>
+        <Col span={8} style={{ display: "flex", justifyContent: "end" }}>
+            {children}
         </Col>
       </Row>
     </Card>
-  )
-}
+  );
+};
 
 export default UsersFilter;
